3b9e12cb8ddab13c5be247b3bc8a7f5600b70b38
Nima Shokri <nimash68@gmail.com>
2020-06-22 16:41:25 +0430

    Add variable sample.

---
+++ samples/variable.php
@@ -0,0 +1,23 @@
+<?php
+
+use \phpWordRtl\phpWordRtl;
+require __DIR__."/../src/phpWordRtl/phpWordRtl.php";
+
+$ds = DIRECTORY_SEPARATOR;
+
+$templatePath =  __DIR__."/template/variable.docx";
+$word = new phpWordRtl($templatePath);
+$word->setVarValue('نام', 'آقای فلانی');
+$word->setVarValue('سازمان', 'سازمان فلان');
+$word->setVarValue('تاریخ', '02/04/1399');
+$word->setVarValue('شماره', '35/858585');
+$word->setVarValue('پیوست', 'ندارد');
+$word->setVarValue('توضیح', 'شرح کار');
+$word->setVarValue('مورد1', 'کالای مورد نیاز اول');
+$word->setVarValue('مورد2', 'کالای مورد نیاز دوم');
+$word->setVarValue('مورد3', 'کالای مورد نیاز سوم');
+$word->setVarValue('نویسنده', 'نام شخص');
+$word->setVarValue('قسمت', 'نام رئیس');
+
+$word->output('aoutput.docx', true);
+

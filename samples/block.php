6467b4d43233f9ed29d842b2ba0769fbf6e03b97
Nima Shokri <nimash68@gmail.com>
2020-06-23 15:02:02 +0430

    Change block sample and template

--- samples/block.php
+++ samples/block.php
@@ -8,7 +8,7 @@ $ds = DIRECTORY_SEPARATOR;
 $templatePath =  __DIR__."/template/block.docx";
 $word = new phpWordRtl($templatePath);
 $word->deleteBlock('بلاک1');
-$word->deleteBlock('بلاک2');
+
 
 $word->output('aoutput.docx', true);
 
